9f88814f8f9cabb4fdcc10aacf6c61391a6e8121
Stephen-exe <watchstephencreate@gmail.com>
2024-11-20 15:44:27 -0500

    added maths & window alerts for ties

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ var losses = 0;
 
 var options = ["R", "P", "S"];
 
-var playGame = function () {
+var playRPS = function () {
   var userChoice = window.prompt("Enter R, P, or S:");
 
   if (!userChoice) {
     return;
   }
-};
+  userChoice = userChoice.toUpperCase();
 
-playGame();
+  var index = Math.floor(Math.random() * options.length);
+  var computerChoice = options[index];
+
+  window.alert("The computer chose " + computerChoice);
+
+  if (userChoice === computerChoice) {
+    ties++;
+    window.alert("It's a tie!");
+
+  };
+
+  playRPS();
