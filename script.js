fa8739991c9452729bfd99153811ab47f0556a1f
Stephen-exe <watchstephencreate@gmail.com>
2024-11-20 16:30:01 -0500

    else if statements on win-loss conditions as well as stats

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var losses = 0;
 var options = ["R", "P", "S"];
 
 var playRPS = function () {
-  var userChoice = window.prompt("Enter R, P, or S:");
+  var userChoice = window.prompt("Type R, P, or S:");
 
   if (!userChoice) {
     return;
@@ -15,12 +15,34 @@ var playRPS = function () {
   var index = Math.floor(Math.random() * options.length);
   var computerChoice = options[index];
 
-  window.alert("The computer chose " + computerChoice);
+  window.alert("Opponent chose " + computerChoice);
 
   if (userChoice === computerChoice) {
     ties++;
-    window.alert("It's a tie!");
+    window.alert(< "Tch, it`s a tie!");
 
-  };
+  } else if (
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")
+  ) {
+    wins++;
+    window.alert("You've won this time..");
 
-  playRPS();
+  } else {
+    losses++;
+    window.alert("Muah haha! You lost!");
+  }
+
+  window.alert(
+    "Stats:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties
+  );
+
+  var playAgain = window.confirm("Play again?");
+
+  if (playAgain) {
+    playRPS();
+  }
+};
+
+playRPS();
