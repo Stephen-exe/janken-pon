e87f344e596a928d9ed83cb0d62451b013bfc238
Stephen-exe <watchstephencreate@gmail.com>
2024-11-20 15:29:43 -0500

    added window prompt for R, P, S

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var ties = 0;
 var losses = 0;
 
 var options = ["R", "P", "S"];
+
+var playGame = function () {
+  var userChoice = window.prompt("Enter R, P, or S:");
+
+  if (!userChoice) {
+    return;
+  }
+};
+
+playGame();
